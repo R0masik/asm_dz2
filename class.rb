f3bd6688cf725aefb655b6619de93352330e2eed
Roman Vorobyov <vorob97@yandex.ru>
2017-05-25 22:27:36 +0300

    1

--- class.rb
+++ class.rb
@@ -6,20 +6,29 @@ class Resolver
 
   def compile
     scan @str, @tokens
-    caseanls @tokens
+    if @tokens.empty?
+      false
+    else
+      caseanls @tokens
+    end
   end
 
   def scan(s, tokens)
-    s.split.map do |el|
+    s.split.each do |el|
       case el
         when 'case', 'of', 'end', ':='
           tokens << el
         else
           case el[-1]
             when ':'
-              el.chop.split(',').each do |_x|
-                tokens << 'num'
-                tokens << ','
+              el.chop.split(',').each do |x|
+                if x.to_i.to_s == x
+                  tokens << 'num'
+                  tokens << ','
+                else
+                  tokens.clear
+                  break
+                end
               end
               tokens.pop
               tokens << ':'
@@ -30,9 +39,14 @@ class Resolver
               if el == ','
                 tokens << el
               else
-                el.chop.split(',').each do |_x|
-                  tokens << 'num'
-                  tokens << ','
+                el.chop.split(',').each do |x|
+                  if x.to_i.to_s == x
+                    tokens << 'num'
+                    tokens << ','
+                  else
+                    tokens.clear
+                    break
+                  end
                 end
               end
             else
@@ -51,9 +65,58 @@ class Resolver
     end
   end
 
-  def caseanls(tokens)
-    state = 1
+  def caseanls (tokens)
+    state = 0
+    tokens.each_with_index do |tok, i|
+      if state == 5 && tok == 'case'
+        state = caseanls(tokens[i..tokens.index('end')])
+      else
+        state = table(state, tok)
+      end
+    end
     state
+  end
+
+  def table(st, tok)
+    case st
+      when 0
+        1 if tok == 'case'
+      when 1
+        2 if tok == 'id'
+      when 2
+        3 if tok == 'of'
+      when 3
+        4 if tok == 'num'
+      when 4
+        if tok == ','
+          3
+        else
+          5 if tok == ':'
+        end
+      when 5
+        6 if tok == 'id'
+      # for tok == 'case' state changes in caseanls
+      when 6
+        7 if tok == ':='
+      when 7
+        8 if tok == 'id' || tok == 'num'
+      when 8
+        if tok == ';'
+          9
+        else
+          true if tok == 'end'
+        end
+      else
+        if tok == 'num'
+          4
+        else
+          if tok == 'end'
+            9
+          else
+            tok == ';'
+          end
+        end
+    end
   end
 
   attr_accessor :str
--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require './class.rb'
 
-str = "case a of 1 , 2, 3: a := 5; 4: case b of 3: b := 4; end; end;"
+str = 'case a of 1 , 2, 3: a := 5; 4: case b of 3: b := 4; end; end;'
 
 main = Resolver.new(str)
 
